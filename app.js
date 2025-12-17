687d12bdd214cf602726cae5781cdc260136b778
ARKuleba <getfun714@gmail.com>
2025-12-18 01:49:01 +0300

    deploy v2

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.get("/health/detailed", async (req, res) => {
 });
 
 app.get("/", (req, res) => {
-  res.json({ message: "Test lab 4! 22222222221" });
+  res.json({ message: "Test lab 4! 333333331" });
 });
 
 require("./app/routes/auth.routes")(app);
