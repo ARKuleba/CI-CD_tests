631de45e0d70247cca45297bf25c717201fea60e
ARKuleba <getfun714@gmail.com>
2025-12-28 22:13:24 +0300

    crash integration

--- test/integration/simple-auth.test.js
+++ test/integration/simple-auth.test.js
@@ -5,7 +5,7 @@ describe("Simple Auth API Tests", () => {
   test("GET / should return welcome message", async () => {
     const response = await request(app).get("/").expect(200);
 
-    expect(response.body.message).toBe("Test lab 4! 400");
+    expect(response.body.message).toBe("Test lab 4! 4000000");
   });
 
   test("GET /health should return status OK", async () => {
