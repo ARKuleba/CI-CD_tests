78b3f681feb9a11e91fc4e768fa2d50cfbe4ac00
ARKuleba <getfun714@gmail.com>
2025-12-28 22:11:16 +0300

    crash unit

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.get("/health/detailed", async (req, res) => {
 });
 
 app.get("/", (req, res) => {
-  res.json({ message: "Test lab 4! 45" });
+  res.json({ message: "Test lab 4! 400" });
 });
 
 require("./app/routes/auth.routes")(app);
